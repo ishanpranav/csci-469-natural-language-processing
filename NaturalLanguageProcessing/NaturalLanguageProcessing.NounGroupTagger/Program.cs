8aca575e82e4cf18e0314c8f22e99b8a3658c594
ishanpranav <ishan.pranav@outlook.com>
2025-10-26 13:41:31 -0400

    Set up homework 5

---
+++ NaturalLanguageProcessing/NaturalLanguageProcessing.NounGroupTagger/Program.cs
@@ -0,0 +1,33 @@
+﻿// Program.cs
+// Copyright (c) 2025 Ishan Pranav
+// Licensed under the MIT license.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace NaturalLanguageProcessing.NounGroupTagger;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Usage: {0} <file>", Process.GetCurrentProcess().ProcessName);
+
+            return;
+        }
+
+        string fileName = args[0];
+
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("File does not exist: \"{0}\".", fileName);
+
+            return;
+        }
+
+
+    }
+}
